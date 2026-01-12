5cb0c1bf80fc0ffec909f5f0a2724c41137fd8a4
lakshaybamel <lakbamel@gmail.com>
2026-01-12 18:04:27 +0530

    Heap: Add remove (extract-min) operation to Min Heap

--- Heap/HeapOperations.java
+++ Heap/HeapOperations.java
@@ -1,8 +1,10 @@
+
 import java.util.*;
 
 public class HeapOperations {
 
     static class Heap {
+
         ArrayList<Integer> arr = new ArrayList<>();
 
         // Insert into Min Heap
@@ -36,8 +38,51 @@ public class HeapOperations {
         }
 
         // Print Peek
-        public void peek() {
-            System.out.println("Peek: " + arr.get(0));
+        public int peek() {
+            return arr.get(0);
+        }
+
+        private void Heapify(int i) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int minIdx = i;
+
+            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
+            }
+
+            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+
+            if(minIdx != i) {
+                // Swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                Heapify(minIdx);
+            }
+        }
+
+        public int remove() {
+            int data = arr.get(0);
+
+            // Step 1: Swap first & last
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size() - 1));
+            arr.set(arr.size() - 1, temp);
+
+            // Step 2: Delete Last
+            arr.remove(arr.size() - 1);
+
+            // Step 3: Heapify
+            Heapify(0);
+            return data;
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
         }
     }
 
@@ -49,7 +94,12 @@ public class HeapOperations {
         h.add(8);
         h.add(1);
 
-        h.printHeap(); // Min Heap
-        h.peek(); // Print Peek
+        // h.printHeap(); // Min Heap
+        // h.peek(); // Print Peek
+
+        while(!h.isEmpty()) { // Heap Sort
+            System.out.println(h.peek());
+            h.remove();
+        }
     }
 }
