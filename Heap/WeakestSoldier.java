44023e501b437f51e494cf5bbb11c314a9e541d7
lakshaybamel <lakbamel@gmail.com>
2026-01-13 19:37:48 +0530

    Heap: Add program to find k weakest soldiers in a matrix

---
+++ Heap/WeakestSoldier.java
@@ -0,0 +1,55 @@
+
+import java.util.PriorityQueue;
+
+public class WeakestSoldier {
+
+    // Row info: number of soldiers + row index
+    static class Row implements Comparable<Row> {
+
+        int soldiers;
+        int idx;
+
+        public Row(int soldiers, int idx) {
+            this.soldiers = soldiers;
+            this.idx = idx;
+        }
+
+        // Min-heap: first by soldiers, then by index
+        @Override
+        public int compareTo(Row r2) {
+            if (this.soldiers == r2.soldiers) {
+                return this.idx - r2.idx;
+            } else {
+                return this.soldiers - r2.soldiers;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int army[][] = {
+            {1, 0, 0, 0},
+            {1, 1, 1, 1},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0}
+        };
+
+        int k = 2;
+
+        PriorityQueue<Row> pq = new PriorityQueue<>();
+
+        // count soldiers in each row
+        for (int i = 0; i < army.length; i++) {
+            int count = 0;
+            for (int j = 0; j < army[0].length; j++) {
+                count += army[i][j];
+            }
+            pq.add(new Row(count, i)); // add to PQ
+        }
+
+        // extract k weakest rows
+        for (int i = 0; i < k; i++) {
+            System.out.println("R" + pq.remove().idx);
+        }
+    }
+}
